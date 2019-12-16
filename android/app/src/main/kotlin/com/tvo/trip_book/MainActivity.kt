f8a8bcb2a5302edcb492f9027dc35c15e934a254
Thomas VOGEL <thomas.vogel.pro@gmail.com>
2019-12-16 17:51:19 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/tvo/trip_book/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tvo.trip_book
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
